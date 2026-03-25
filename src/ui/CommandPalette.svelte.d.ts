285319055af747469ac509b9990857c8f173daad
ADJ189 <funnyhut3@gmail.com>
2026-03-25 13:19:55 +0530

    Delete src/ui/CommandPalette.svelte.d.ts

--- src/ui/CommandPalette.svelte.d.ts
+++
@@ -1,6 +0,0 @@
-import { SvelteComponent } from 'svelte';
-export interface CommandPaletteProps {
-  onAction?: (keyword: string) => void;
-  onRandomTheme?: () => void;
-}
-export default class CommandPalette extends SvelteComponent<CommandPaletteProps> {}
